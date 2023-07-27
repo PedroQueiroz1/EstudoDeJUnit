af0ded666beb848739df72fef26a4bcff3d0398e
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-07-27 15:52:35 -0300

    Adiciona novas anotacoes e metodos

--- JUnit/src/test/java/br/com/junit/MathUtilsTest.java
+++ JUnit/src/test/java/br/com/junit/MathUtilsTest.java
@@ -1,20 +1,41 @@
 package br.com.junit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(value = TestInstance.Lifecycle.PER_METHOD)
 class MathUtilsTest {
+
+    MathUtils mathUtils;
+
+    @BeforeAll
+    static void beforeAllInit() {
+        System.out.println("This needs to run before all");
+    }
+
+    @AfterAll
+    static void afterAllInit() {
+        System.out.println("This needs to run after all");
+    }
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanup() {
+        System.out.println("Cleaning up...");
+    }
 
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();int expected = 2;
+        int expected = 2;
         int actual = mathUtils.add(1, 1);
         Assertions.assertEquals(expected, actual, "The add method should add two numbers");
     }
 
     @Test
     void testSubtract() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.subtract(4, 2);
         Assertions.assertEquals(expected, actual, "The subtract method should subtract two numbers");
@@ -22,7 +43,6 @@ class MathUtilsTest {
 
     @Test
     void testMultiply() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.multiply(2, 1);
         Assertions.assertEquals(expected, actual, "The multiply method should multiply two numbers");
@@ -30,14 +50,12 @@ class MathUtilsTest {
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         Assertions.assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
                 "Divide by zero should throw");
     }
 
     @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         Assertions.assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),
                 "Should return right circle area");
     }
